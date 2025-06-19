854b065232b51f550a3da47f34296832f01edb85
Daniel Montes <gdanielmontesh9@gmail.com>
2025-06-19 16:21:46 -0600

    build: add and config lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "**/*.js": ["prettier --write"],
+    "**/*.ts": ["prettier --write"],
+    "**/*.tsx": ["prettier --write"],
+    "**/*.jsx": ["prettier --write"],
+    "**/*.json": ["prettier --write"],
+    "**/*.css": ["prettier --write"],
+    "**/*.scss": ["prettier --write"],
+    "**/*.md": ["prettier --write"]
+};
