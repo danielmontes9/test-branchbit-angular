72e272c10a57a2493ca79f9b5b885dcd8985bd87
Daniel Montes <gdanielmontesh9@gmail.com>
2025-06-19 16:30:15 -0600

    feat: add navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+@Component({
+	selector: "app-navbar",
+	imports: [],
+	templateUrl: "./navbar.component.html",
+	styleUrl: "./navbar.component.scss",
+	imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class NavbarComponent {}
