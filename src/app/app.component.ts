d9ebd570137a9b9c2fbb633f422d417058350344
Daniel Montes <gdanielmontesh9@gmail.com>
2025-06-19 16:46:12 -0600

    fix: show angular material styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	imports: [NavbarComponent, RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'test-branchbit-angular';
+	title = "test-branchbit-angular";
 }
