7d95b4f626f732065afe763f8c4df214b1abb2ab
Daniel Montes <gdanielmontesh9@gmail.com>
2025-06-19 19:41:23 -0600

    refactor: data random images

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -9,6 +9,7 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { ImageComponent } from "../shared/image/image.component";
 import { NgFor } from "@angular/common";
 import { SurveyComponent } from "../shared/survey/survey.component";
+import { DATA_RANDOM_IMAGES } from "../../core/data/DATA_RANDOM_IMAGES";
 
 @Component({
 	selector: "app-content",
@@ -49,90 +50,5 @@ export class ContentComponent {
 		],
 	};
 
-	dataTabsImages = {
-		tab1: [
-			{
-				imageUrl: "https://picsum.photos/201",
-				altText: "Placeholder Image 1",
-			},
-			{
-				imageUrl: "https://picsum.photos/202",
-				altText: "Placeholder Image 2",
-			},
-			{
-				imageUrl: "https://picsum.photos/203",
-				altText: "Placeholder Image 3",
-			},
-		],
-		tab2: [
-			{
-				imageUrl: "https://picsum.photos/204",
-				altText: "Placeholder Image 4",
-			},
-			{
-				imageUrl: "https://picsum.photos/205",
-				altText: "Placeholder Image 5",
-			},
-			{
-				imageUrl: "https://picsum.photos/206",
-				altText: "Placeholder Image 6",
-			},
-		],
-		tab3: [
-			{
-				imageUrl: "https://picsum.photos/207",
-				altText: "Placeholder Image 7",
-			},
-			{
-				imageUrl: "https://picsum.photos/208",
-				altText: "Placeholder Image 8",
-			},
-			{
-				imageUrl: "https://picsum.photos/209",
-				altText: "Placeholder Image 9",
-			},
-		],
-		tab4: [
-			{
-				imageUrl: "https://picsum.photos/210",
-				altText: "Placeholder Image 10",
-			},
-			{
-				imageUrl: "https://picsum.photos/211",
-				altText: "Placeholder Image 11",
-			},
-			{
-				imageUrl: "https://picsum.photos/212",
-				altText: "Placeholder Image 12",
-			},
-		],
-		tab5: [
-			{
-				imageUrl: "https://picsum.photos/213",
-				altText: "Placeholder Image 13",
-			},
-			{
-				imageUrl: "https://picsum.photos/214",
-				altText: "Placeholder Image 14",
-			},
-			{
-				imageUrl: "https://picsum.photos/215",
-				altText: "Placeholder Image 15",
-			},
-		],
-		tab6: [
-			{
-				imageUrl: "https://picsum.photos/216",
-				altText: "Placeholder Image 16",
-			},
-			{
-				imageUrl: "https://picsum.photos/217",
-				altText: "Placeholder Image 17",
-			},
-			{
-				imageUrl: "https://picsum.photos/218",
-				altText: "Placeholder Image 18",
-			},
-		],
-	};
+	dataTabsImages = DATA_RANDOM_IMAGES;
 }
---
+++ src/app/core/data/DATA_RANDOM_IMAGES.ts
@@ -0,0 +1,86 @@
+export const DATA_RANDOM_IMAGES = {
+	tab1: [
+		{
+			imageUrl: "https://picsum.photos/201",
+			altText: "Placeholder Image 1",
+		},
+		{
+			imageUrl: "https://picsum.photos/202",
+			altText: "Placeholder Image 2",
+		},
+		{
+			imageUrl: "https://picsum.photos/203",
+			altText: "Placeholder Image 3",
+		},
+	],
+	tab2: [
+		{
+			imageUrl: "https://picsum.photos/204",
+			altText: "Placeholder Image 4",
+		},
+		{
+			imageUrl: "https://picsum.photos/205",
+			altText: "Placeholder Image 5",
+		},
+		{
+			imageUrl: "https://picsum.photos/206",
+			altText: "Placeholder Image 6",
+		},
+	],
+	tab3: [
+		{
+			imageUrl: "https://picsum.photos/207",
+			altText: "Placeholder Image 7",
+		},
+		{
+			imageUrl: "https://picsum.photos/208",
+			altText: "Placeholder Image 8",
+		},
+		{
+			imageUrl: "https://picsum.photos/209",
+			altText: "Placeholder Image 9",
+		},
+	],
+	tab4: [
+		{
+			imageUrl: "https://picsum.photos/210",
+			altText: "Placeholder Image 10",
+		},
+		{
+			imageUrl: "https://picsum.photos/211",
+			altText: "Placeholder Image 11",
+		},
+		{
+			imageUrl: "https://picsum.photos/212",
+			altText: "Placeholder Image 12",
+		},
+	],
+	tab5: [
+		{
+			imageUrl: "https://picsum.photos/213",
+			altText: "Placeholder Image 13",
+		},
+		{
+			imageUrl: "https://picsum.photos/214",
+			altText: "Placeholder Image 14",
+		},
+		{
+			imageUrl: "https://picsum.photos/215",
+			altText: "Placeholder Image 15",
+		},
+	],
+	tab6: [
+		{
+			imageUrl: "https://picsum.photos/216",
+			altText: "Placeholder Image 16",
+		},
+		{
+			imageUrl: "https://picsum.photos/217",
+			altText: "Placeholder Image 17",
+		},
+		{
+			imageUrl: "https://picsum.photos/218",
+			altText: "Placeholder Image 18",
+		},
+	],
+};
